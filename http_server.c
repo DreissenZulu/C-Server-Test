2223bcb589c2e51ee8138553526a9a5940054055
Alex Khoa Le <leduykhoa@hotmail.ca>
2020-03-03 15:35:21 -0500

    Server now analyzes request from browser and returns html and css

--- http_server.c
+++ http_server.c
@@ -11,16 +11,6 @@ int main()
         WSAStartup(versionWanted, &wsaData);
     }
 
-    // Read and put the html page, index.html, into memory
-    FILE *html_page = fopen("./index.html", "r");
-    char html_string[600];
-    char http_header[2048] = "HTTP/1.1 200 OK\r\n\r\n";
-    fgets(html_string, sizeof(html_string), html_page);
-
-    // Response header tells the client that the request was received successfully
-    // We make the header large so that we can hold the html_string too
-    strcat(http_header, html_string);
-
     SOCKET server_socket;
     struct sockaddr_in server_address;
     int addr_len = sizeof(server_address);
@@ -44,6 +34,7 @@ int main()
     bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address));
 
     listen(server_socket, 3);
+    printf("Awaiting connections on port %d...\n", ntohs(server_address.sin_port));
 
     // The last two parameters in accept are set to NULL since we don't need to save the client's address
     while (1)
@@ -57,12 +48,41 @@ int main()
         printf("Connection established at %d\n", ntohs(server_address.sin_port));
         char buffer[2000] = {0};
         valread = recv(client_socket, buffer, 2000, 0);
-        printf("%s\n", buffer);
 
-        // Send the server_message string to the client at client_socket
-        send(client_socket, http_header, strlen(http_header), 0);
+        if (memcmp(buffer, "GET", 3) == 0)
+        {
+            char data_string[600] = {0};
+            char http_header[2048] = "HTTP/1.1 200 OK\r\n";
+            if (strstr(buffer, "text/css") != NULL)
+            {
+                printf("Requesting .css file!\n");
+                FILE *css_style = fopen("./assets/style.css", "r");
+                strcat(http_header, "Content-Type: text/css;\r\n\r\n");
+                fgets(data_string, sizeof(data_string), css_style);
 
-        printf("Data sent to client\n");
+                strcat(http_header, data_string);
+            }
+            else if (strstr(buffer, "text/html") != NULL)
+            {
+                printf("Requesting .html file!\n");
+                // Read and put the html page, index.html, into memory
+                FILE *html_page = fopen("./index.html", "r");
+                strcat(http_header, "Content-Type: text/html; charset: UTF-8\r\n\r\n");
+                fgets(data_string, sizeof(data_string), html_page);
+
+                // Response header tells the client that the request was received successfully
+                // We make the header large so that we can hold the html_string too
+                strcat(http_header, data_string);
+            }
+            else
+            {
+                printf("No compatible file requested...");
+            }
+            // Send the server_message string to the client at client_socket
+            send(client_socket, http_header, strlen(http_header), 0);
+            printf("Data sent to client\n");
+        }
+
         shutdown(client_socket, SD_SEND);
     }
 
